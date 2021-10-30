b7cc9a1f6f9d813281c0a77188804a45f2672f6c
Mwein <mweinberg21@gmail.com>
2021-10-30 01:00:34 -0500

    Update settings controller

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const { accountInfoController } = require('./controllers/getAccountInfo')
 const { getLogsController } = require('./controllers/getLogs')
 const { getGainLossController } = require('./controllers/getGainLoss')
-const { getSettingsController } = require('./controllers/getSettings')
+
+const settings = require('./controllers/settings')
 const watchlist = require('./controllers/watchlist')
 
-app.get('/ping', (req, res) => {
-  res.send('pong')
-})
+app.use(bodyParser.json())
+
+app.get('/logs', getLogsController)
 
 app.get('/account-info', accountInfoController)
-app.get('/logs', getLogsController)
 app.get('/gain-loss', getGainLossController)
-app.get('/settings', getSettingsController)
+
+// Settings
+app.get('/settings', settings.getSettingsController)
+app.put('/settings', settings.setSettingsController)
+
+// Watchlist
 app.get('/watchlist', watchlist.getWatchlistController)
 
 module.exports = app
--- controllers/settings.js
+++ controllers/settings.js
@@ -8,6 +8,22 @@ const defaultSettings = {
   buyToCloseAmount: 1, // Limit price when making automated buy_to_close orders
 }
 
+
+const _getSettings = async () => {
+  const mongoSettings = await settingSchema.find()
+
+  // Replace default settings if needed
+  const settings = mongoSettings.reduce((acc, setting) => (
+    {
+      ...acc,
+      [setting.key]: setting.value
+    }
+  ), defaultSettings)
+  
+  return settings
+}
+
+
 const getSettingsController = async (req, res) => {
   try {
     const mongoSettings = await settingSchema.find()
@@ -26,7 +42,25 @@ const getSettingsController = async (req, res) => {
   }
 }
 
+
+const setSettingsController = async (req, res) => {
+  try {
+    await Promise.all(Object.keys(req.body).map(async key => {
+      const value = req.body[key]
+      await settingSchema.findOneAndUpdate({ key }, { key, value }, { upsert: true })
+    }))
+  
+    const newSettings = await _getSettings()
+    res.json(newSettings)
+  } catch (e) {
+    res.status(500).send('Error')
+  }
+}
+
+
 module.exports = {
   defaultSettings,
+  _getSettings,
   getSettingsController,
+  setSettingsController,
 }
