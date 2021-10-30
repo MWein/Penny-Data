350a22fe5610a453f5cb032a386d3420110b3bdc
Mwein <mweinberg21@gmail.com>
2021-10-29 21:54:30 -0500

    Copied over modified network helper from Penny

---
+++ utils/network.js
@@ -0,0 +1,58 @@
+const superagent = require('superagent')
+const NodeCache = require( "node-cache" )
+const getCache = new NodeCache()
+
+
+// Generate form string from object
+// Superagent doesn't handle this without multiple sends
+const _createFormString = body => Object.keys(body).map(key => {
+  const value = body[key]
+  const formattedValue = Array.isArray(value) ? value.join(',') : value
+  return `${key}=${formattedValue}`
+}).join('&')
+
+
+const get = async (path) => {
+  const url = `${process.env.BASEPATH}${path}`
+
+  const cachedResponse = getCache.get(url)
+  if (cachedResponse) {
+    return cachedResponse
+  }
+
+  const response = await superagent.get(url)
+    .set('Authorization', `Bearer ${process.env.APIKEY}`)
+    .set('Accept', 'application/json')
+    .timeout({
+      response: 5000
+    })
+    .retry(5)
+
+  // Cache for 15 minutes
+  getCache.set(url, cachedResponse, 900)
+
+  return response.body
+}
+
+
+const post = async (path, body) => {
+  const url = `${process.env.BASEPATH}${path}`
+  const formString = _createFormString(body)
+
+  const response = await superagent.post(url)
+    .set('Authorization', `Bearer ${process.env.APIKEY}`)
+    .set('Accept', 'application/json')
+    .send(formString)
+    .timeout({
+      response: 10000
+    })
+    .retry(5)
+
+  return response.body
+}
+
+module.exports = {
+  _createFormString,
+  get,
+  post,
+}
