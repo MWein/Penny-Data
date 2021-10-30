97bc2c567d35af076e180b7bfba1cd20af5a0531
Mwein <mweinberg21@gmail.com>
2021-10-30 01:12:30 -0500

    Added details to gain loss

--- controllers/getGainLoss.js
+++ controllers/getGainLoss.js
@@ -1,9 +1,36 @@
 const gainLossSchema = require('../db_models/gainLossSchema')
+const {
+  isOption,
+  determineOptionTypeFromSymbol
+} = require('../utils/determineOptionType')
 
 const getGainLossController = async (req, res) => {
   try {
-    const gainLossData = await gainLossSchema.find().sort({ close_date: -1 }).select('-_id -__v -hashId')
-    res.json(gainLossData)
+    const gainLossData = await gainLossSchema
+      .find()
+      .sort({ close_date: -1 })
+      .select('-_id -__v -hashId')
+    
+    const callOptions = gainLossData.filter(gl =>
+      determineOptionTypeFromSymbol(gl.symbol) === 'call')
+    const putOptions = gainLossData.filter(gl =>
+      determineOptionTypeFromSymbol(gl.symbol) === 'call')
+    const stocks = gainLossData.filter(gl => !isOption(gl.symbol))
+
+    const callGL = callOptions.reduce((acc, opt) => acc + opt.gain_loss, 0)
+    const putGL = putOptions.reduce((acc, opt) => acc + opt.gain_loss, 0)
+    const optionGL = callGL + putGL
+    const stockGL = stocks.reduce((acc, opt) => acc + opt.gain_loss, 0)
+    const totalGL = stockGL + optionGL
+
+    res.json({
+      callGL,
+      putGL,
+      optionGL,
+      stockGL,
+      totalGL,
+      gainLossData
+    })
   } catch (e) {
     res.status(500).send('Error')
   }
