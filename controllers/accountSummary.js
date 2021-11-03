44cdd906c2d986c29baaf04b0c0c6454813cc1b9
Mwein <mweinberg21@gmail.com>
2021-11-02 19:28:42 -0500

    Returns combined open contracts regardless of strike

--- controllers/accountSummary.js
+++ controllers/accountSummary.js
@@ -3,6 +3,17 @@ const positionUtil = require('../tradier/getPositions')
 const { isOption, determineOptionTypeFromSymbol, getUnderlying } = require('../utils/determineOptionType')
 const gainLossService = require('./gainLoss')
 const quotesUtil = require('../tradier/getQuotes')
+const ordersUtil = require('../tradier/getOrders')
+
+
+const getCoveredText = (btcOrders, numPositions) => {
+  if (btcOrders === numPositions) {
+    return 'yes'
+  } else if (btcOrders === 0) {
+    return 'no'
+  }
+  return 'partially'
+}
 
 const accountSummaryController = async (req, res) => {
   try {
@@ -12,11 +23,13 @@ const accountSummaryController = async (req, res) => {
 
     const [
       balances,
+      orders,
       openPositions,
       monthGainLoss,
       yearGainLoss,
     ] = await Promise.all([
       balancesUtil.getBalances(),
+      ordersUtil.getOrders(),
       positionUtil.getPositions(),
       gainLossService.getGainLoss(firstOfMonth, today),
       gainLossService.getGainLoss(firstOfYear, today),
@@ -24,18 +37,38 @@ const accountSummaryController = async (req, res) => {
 
     const options = openPositions.filter(pos => isOption(pos.symbol))
     const optionsTickers = options.map(opt => opt.symbol)
+    const buyToCloseOrders = ordersUtil.filterForOptionBuyToCloseOrders(orders)
+
     const quotes = await quotesUtil.getQuotes(optionsTickers)
 
-    const optionsContracts = options.map(pos => {
+    const optionsContracts = Object.values(options.map(pos => {
       return {
         symbol: getUnderlying(pos.symbol),
         type: determineOptionTypeFromSymbol(pos.symbol),
         contracts: pos.quantity * -1,
-        covered: 'no', // TODO
-        nextExpiration: new Date(quotes.find(quote => quote.symbol === pos.symbol).expiration_date),
+        nextExpiration: quotes.find(quote => quote.symbol === pos.symbol).expiration_date,
         premium: pos.cost_basis * -1,
       }
-    })
+    }).reduce((acc, contract) => {
+      const key = `${contract.symbol}${contract.type}${contract.nextExpiration}`
+      const old = acc[key] || { contracts: 0, premium: 0 }
+
+      const totalBuyToCloseOrders = buyToCloseOrders
+        .filter(x => x.symbol === contract.symbol)
+        .reduce((acc, x) => acc + x.quantity, 0)
+
+      const newContracts = contract.contracts + old.contracts
+
+      return {
+        ...acc,
+        [key]: {
+          ...contract,
+          contracts: newContracts,
+          premium: contract.premium + old.premium,
+          covered: getCoveredText(totalBuyToCloseOrders, newContracts)
+        }
+      }
+    }, {}))
 
 
     const monthOptionProfit = monthGainLoss.optionGL
