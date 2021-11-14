27bf6ef4108fe16ba08aa9fae6ba3540d5cad025
Mwein <mweinberg21@gmail.com>
2021-11-13 18:02:11 -0600

    Added comments to explain endpoints

--- app.js
+++ app.js
@@ -16,16 +16,25 @@ const watchlist = require('./controllers/watchlist')
 app.use(bodyParser.json())
 app.use(cors())
 
-// Actually used!
+
+// Status endpoint that checks the last action by Penny via the logs database
 app.get('/penny-status', pennyStatusController)
+
+// Balances, Orders, Positions, Month Gain, Year Gain
 app.get('/account-summary', accountSummaryController)
+
+// Watchlist Endpoints
 app.get('/watchlist', watchlist.getWatchlistController)
+
+// Dump all logs from the database
 app.get('/logs', getLogsController)
+
 
 app.get('/gain-loss', gainLoss.getGainLossController)
 app.get('/gain-loss-graph', gainLoss.getGainLossGraphController)
-
 app.get('/position-history-graph', positionGraph.getPositionGraphDataController)
+
+
 
 // Settings
 app.get('/settings', settings.getSettingsController)
