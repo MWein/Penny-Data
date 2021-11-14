052d678cbf8cac76ad01352d9b7884fec165fafe
Mwein <mweinberg21@gmail.com>
2021-11-13 18:51:00 -0600

    Split settings into services

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,41 +1,9 @@
-const settingSchema = require('../db_models/settingSchema')
-
-const defaultSettings = {
-  callsEnabled: true,
-  putsEnabled: true,
-  maxAllocation: 4000, // The maximum amount of money to put down on a single ticker
-  reserve: 0, // Money that Penny shouldn't touch. BuyingPower - Reserve. For planned withdrawals.
-  buyToCloseAmount: 1, // Limit price when making automated buy_to_close orders
-}
-
-
-const _getSettings = async () => {
-  const mongoSettings = await settingSchema.find()
-
-  // Replace default settings if needed
-  const settings = mongoSettings.reduce((acc, setting) => (
-    {
-      ...acc,
-      [setting.key]: setting.value
-    }
-  ), defaultSettings)
-  
-  return settings
-}
+const settingsService = require('../services/settings')
 
 
 const getSettingsController = async (req, res) => {
   try {
-    const mongoSettings = await settingSchema.find()
-
-    // Replace default settings if needed
-    const settings = mongoSettings.reduce((acc, setting) => (
-      {
-        ...acc,
-        [setting.key]: setting.value
-      }
-    ), defaultSettings)
-  
+    const settings = await settingsService.getSettings()
     res.json(settings)
   } catch (e) {
     res.status(500).send('Error')
@@ -45,12 +13,7 @@ const getSettingsController = async (req, res) => {
 
 const setSettingsController = async (req, res) => {
   try {
-    await Promise.all(Object.keys(req.body).map(async key => {
-      const value = req.body[key]
-      await settingSchema.findOneAndUpdate({ key }, { key, value }, { upsert: true })
-    }))
-  
-    const newSettings = await _getSettings()
+    const newSettings = await settingsService.setSettings(req.body)
     res.json(newSettings)
   } catch (e) {
     res.status(500).send('Error')
@@ -59,8 +22,6 @@ const setSettingsController = async (req, res) => {
 
 
 module.exports = {
-  defaultSettings,
-  _getSettings,
   getSettingsController,
   setSettingsController,
 }
