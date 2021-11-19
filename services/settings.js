1fb15f8720ceca9efc77587aedb159bcf4c9972e
Mwein <mweinberg21@gmail.com>
2021-11-18 18:04:51 -0600

    copied default settings from Penny

--- services/settings.js
+++ services/settings.js
@@ -4,8 +4,9 @@ const defaultSettings = {
   callsEnabled: true,
   putsEnabled: true,
   maxAllocation: 4000, // The maximum amount of money to put down on a single ticker
+  maxPositions: 5, // The maximum number of positions any one position can have
   reserve: 0, // Money that Penny shouldn't touch. BuyingPower - Reserve. For planned withdrawals.
-  buyToCloseAmount: 1, // Limit price when making automated buy_to_close orders
+  buyToCloseAmount: 0.01, // Limit price when making automated buy_to_close orders
 }
 
 
